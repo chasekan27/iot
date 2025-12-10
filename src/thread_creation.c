cb9ca85a7f1d4c3f1178d944c4cf8e2e57ab282b
chasekan27 <chasekan27@gmail.com>
2025-12-10 07:19:04 +0530

    thread is defined with the required comments

--- src/thread_creation.c
+++ src/thread_creation.c
@@ -11,15 +11,15 @@ static struct k_thread nodemcu8266_thread;
 unsigned int create_thread(void)
 {
     /* Create WiFi init thread */
-    k_thread_create(
-        &nodemcu8266_thread,             // thread object
-        nodemcu8266_stack,                       // stack
-        K_THREAD_STACK_SIZEOF(nodemcu8266_stack),
-        init_nodemcu8266_module,                             // thread entry
-        NULL, NULL, NULL,                             // parameters
-        NODEMCU8266_THREAD_PRIO,                             // priority
-        0,                                            // options
-        K_NO_WAIT                                     // start immediately
-    );
+k_thread_create(
+    &nodemcu8266_thread,                 // Thread control block (k_thread object)
+    nodemcu8266_stack,                   // Pointer to thread stack memory
+    K_THREAD_STACK_SIZEOF(nodemcu8266_stack),  // Size of the stack
+    init_nodemcu8266_module,             // Thread entry function
+    NULL, NULL, NULL,                    // Arguments passed to the entry function
+    NODEMCU8266_THREAD_PRIO,             // Thread priority
+    0,                                   // Thread options (e.g., cooperativity)
+    K_NO_WAIT                            // Start immediately after creation
+);
     return 0;
 }
